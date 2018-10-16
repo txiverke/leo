5a5ca468a3f4c0f6f14c0b586f853ae3911b508a
Carles11 <c.delriofrances@gmail.com>
2018-10-16 22:22:31 +0200

    Update v:0.2.2 - add key dictionary and update header comp

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,34 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from "react"
+import PropTypes from "prop-types"
 
-import Background from './Background'
-import bg from '../assets/imgs/bg.jpg'
-import logo from '../assets/imgs/logo.png'
+import Background from "./Background"
+import bg from "../assets/imgs/bg.jpg"
+import logo from "../assets/imgs/logo.png"
 
-const CSS_NAME = 'app-header-bg'
+const CSS_NAME = "app-header-bg"
 
-const Header = (props) => {
+const Header = props => {
   const { DIC } = props
 
   return (
     <header className="app-section h725">
       <div className="app-header">
-        <h1 className="tit-header mb2rem">
-          {DIC.HEADER_MAIN}
-        </h1>
+        <h1 className="tit-header mb2rem">{DIC.HEADER_MAIN}</h1>
         <img src={logo} alt={DIC.HEADER_MAIN} />
-        <h2 className="subtit-header">
-          {DIC.DESCRIPTION}
-        </h2>
+        <h2 className="subtit-header">{DIC.DESCRIPTION}</h2>
+        <h3>{DIC.SUBDESCRIPTION}</h3>
       </div>
-      <Background css={CSS_NAME} url={bg} label={`${DIC.HEADER_MAIN}, ${DIC.DESCRIPTION}`} />
+      <Background
+        css={CSS_NAME}
+        url={bg}
+        label={`${DIC.HEADER_MAIN}, ${DIC.DESCRIPTION}`}
+      />
     </header>
   )
 }
 
 Header.propTypes = {
-  DIC: PropTypes.object.isRequired,
+  DIC: PropTypes.object.isRequired
 }
 
 export default Header
--- src/utils/dictionary.js
+++ src/utils/dictionary.js
@@ -1,55 +1,58 @@
 const dictionary = {
   es: {
-    CONCURSO: 'El Concurso',
+    CONCURSO: "El Concurso",
     CONCURSO_TXT:
-			'La lectura nos ayuda a comunicarnos con nuestro entorno y los concursos motivan a alumnos y profesores de todos los ámbitos de la enseñanza a conseguir nuevas metas. En este contexto surgió en 2012 la idea de poner en marcha un concurso de lectura en español en Hesse. Tras una fase de prueba se ha ido consolidando la iniciativa obteniendo en 2015 el apoyo del Kultusministerium de Hesse y entrando a formar parte de la red oficial de concursos escolares en esta Comunidad Autónoma. La trayectoria del concurso desde 2012 ha demostrado la gran acogida del concurso por parte de los centros educativos.Esto se ha debido especialmente a la fácil organización del concurso en la clase de español. Todos los alumnos de secundario en la enseñanza reglada del sistema educativo alemán con español como segunda y tercera lengua pueden participar en el concurso, que se desarrolla en cuatro categorías de A1 a B2, según el Marco Común Europeo de Referencia para las lenguas(MCER).',
-    CONCURSO_EDICION: '¿Cuándo es la próxima edición?',
+      "La lectura nos ayuda a comunicarnos con nuestro entorno y los concursos motivan a alumnos y profesores de todos los ámbitos de la enseñanza a conseguir nuevas metas. En este contexto surgió en 2012 la idea de poner en marcha un concurso de lectura en español en Hesse. Tras una fase de prueba se ha ido consolidando la iniciativa obteniendo en 2015 el apoyo del Kultusministerium de Hesse y entrando a formar parte de la red oficial de concursos escolares en esta Comunidad Autónoma. La trayectoria del concurso desde 2012 ha demostrado la gran acogida del concurso por parte de los centros educativos.Esto se ha debido especialmente a la fácil organización del concurso en la clase de español. Todos los alumnos de secundario en la enseñanza reglada del sistema educativo alemán con español como segunda y tercera lengua pueden participar en el concurso, que se desarrolla en cuatro categorías de A1 a B2, según el Marco Común Europeo de Referencia para las lenguas(MCER).",
+    CONCURSO_EDICION: "¿Cuándo es la próxima edición?",
     CONCURSO_EDICION_TXT:
-			'La Final de la octava convocatoria del Concurso Escolar de Lectura en Español ‟Leo, leo… ¿Qué lees?” tendrá lugar en el Salón de Actos de la sede del Instituto Cervantes de Fráncfort el día 16 de mayo de 2019 en la sede de Fráncfort del Instituto Cervantes. La fecha límite para la inscripción de los colegios participantes será el 30 de noviembre de 2018.',
-    CONCURSO_ORGANIZA: '¿Quién lo organiza?',
+      "La Final de la octava convocatoria del Concurso Escolar de Lectura en Español ‟Leo, leo… ¿Qué lees?” tendrá lugar en el Salón de Actos de la sede del Instituto Cervantes de Fráncfort el día 16 de mayo de 2019 en la sede de Fráncfort del Instituto Cervantes. La fecha límite para la inscripción de los colegios participantes será el 30 de noviembre de 2018.",
+    CONCURSO_ORGANIZA: "¿Quién lo organiza?",
     CONCURSO_ORGANIZA_TXT:
-			'Siguiendo la larga tradición de los concursos de lectura en Alemania el Concurso Escolar de Lectura en Español ‟Leo, leo… ¿Qué lees ?” nació como propuesta educativa en el colegio Hessenwaldschule (Weiterstadt) para dar respuesta a un vacío de iniciativas que reforzaran el aprendizaje de la lengua española.',
-    CONCURSO_PARTICIPAR: '¿Quién puede participar?',
+      "Siguiendo la larga tradición de los concursos de lectura en Alemania el Concurso Escolar de Lectura en Español ‟Leo, leo… ¿Qué lees ?” nació como propuesta educativa en el colegio Hessenwaldschule (Weiterstadt) para dar respuesta a un vacío de iniciativas que reforzaran el aprendizaje de la lengua española.",
+    CONCURSO_PARTICIPAR: "¿Quién puede participar?",
     CONCURSO_PARTICIPAR_TXT:
-			'Todos los alumnos de secundario en la enseñanza reglada del sistema educativo alemán con español como segunda y tercera lengua pueden participar en el concurso, que se desarrolla en cuatro categorías de A1 a B2, según el Marco Común Europeo de Referencia para las lenguas (MCER).',
-    CONCURSO_PATROCINA: '¿Quién nos patrocina?',
+      "Todos los alumnos de secundario en la enseñanza reglada del sistema educativo alemán con español como segunda y tercera lengua pueden participar en el concurso, que se desarrolla en cuatro categorías de A1 a B2, según el Marco Común Europeo de Referencia para las lenguas (MCER).",
+    CONCURSO_PATROCINA: "¿Quién nos patrocina?",
     CONCURSO_PATROCINA_TXT:
-			'En su octavo año de celebración consecutiva cuenta con el apoyo institucional de la Consejería de Educación de la Embajada de España en Alemania, del Instituto Cervantes de Fráncfort, del Consulado General de España en Fráncfort, del Consulado General de México en Fráncfort, del Consulado General de Colombia en Fráncfort, del Consulado General de Chile en Fráncfort y de la Asociación Alemana de Profesores de Español en Hesse (DSV). Asímismo con el patrocinio de las editoriales Grupo SM, Schulverlag Klett, Cornelsen Verlag y Edinumen.',
-    DESCRIPTION: 'Concurso de lectura en español.',
-    ERROR_EMAIL: 'debe ser un correo electronico válido.',
-    ERROR_REQUIRED: 'es un campo obligatorio.',
-    ERROR_TEXT: 'debe ser mas de 6 caracteres',
-    FORM_ADDRESS: 'Dirección',
-    FORM_CATEGORIES: 'Selecciona las categorias del concurso en las que participará el colegio:',
-    FORM_CONTACT: 'Persona de contacto',
-    FORM_COURSE: 'Curso',
-    FORM_MAIL: 'Correo electrónico',
-    FORM_PHONE: 'Teléfono',
-    FORM_SCHOOL_NAME: 'Nombre del colegio',
-    FORM_TITLE: 'Formulario de inscripción',
-    FORM_SUBTITLE: 'Registra tu colegio',
-    HEADER_MAIN: 'Leo, leo... ¿qué lees?',
+      "En su octavo año de celebración consecutiva cuenta con el apoyo institucional de la Consejería de Educación de la Embajada de España en Alemania, del Instituto Cervantes de Fráncfort, del Consulado General de España en Fráncfort, del Consulado General de México en Fráncfort, del Consulado General de Colombia en Fráncfort, del Consulado General de Chile en Fráncfort y de la Asociación Alemana de Profesores de Español en Hesse (DSV). Asímismo con el patrocinio de las editoriales Grupo SM, Schulverlag Klett, Cornelsen Verlag y Edinumen.",
+    DESCRIPTION: "Concurso de lectura en español.",
+    SUBDESCRIPTION: "Próxima final el 16 de mayo de 2019",
+    ERROR_EMAIL: "debe ser un correo electronico válido.",
+    ERROR_REQUIRED: "es un campo obligatorio.",
+    ERROR_TEXT: "debe ser mas de 6 caracteres",
+    FORM_ADDRESS: "Dirección",
+    FORM_CATEGORIES:
+      "Selecciona las categorias del concurso en las que participará el colegio:",
+    FORM_CONTACT: "Persona de contacto",
+    FORM_COURSE: "Curso",
+    FORM_MAIL: "Correo electrónico",
+    FORM_PHONE: "Teléfono",
+    FORM_SCHOOL_NAME: "Nombre del colegio",
+    FORM_TITLE: "Formulario de inscripción",
+    FORM_SUBTITLE: "Registra tu colegio",
+    HEADER_MAIN: "Leo, leo... ¿qué lees?",
     MSG_SCHOOL_CREATED:
-			'Gracias por inscribirse al concurso de lectura en español "Leo, leo... ¿qué lees?". En breve nos pondremos en contacto con usted.',
-    MSG_SCHOOL_ERROR: 'Lamentablemente no se pudo enviar el formulario, inténtelo más tarde.',
-    MORE_INFO: 'Más información',
-    NAME: 'Leo, leo',
-    NAV_BASES: 'Bases del concurso',
-    NAV_CERTIFICADOS: 'Certificados',
-    NAV_COLEGIOS: 'Colegios inscritos',
-    NAV_CONCURSO: 'Concurso',
-    NAV_GALERIA: 'Galeria',
-    NAV_CRITERIOS: 'Criterios de lectura',
-    NAV_INFO: 'Información para profesores',
-    NAV_INSCRIPCION: 'Inscripción',
-    NAV_TEXTOS: 'Textos de lectura',
-    NOTFOUND: 'No se ha encontrado la página.',
+      'Gracias por inscribirse al concurso de lectura en español "Leo, leo... ¿qué lees?". En breve nos pondremos en contacto con usted.',
+    MSG_SCHOOL_ERROR:
+      "Lamentablemente no se pudo enviar el formulario, inténtelo más tarde.",
+    MORE_INFO: "Más información",
+    NAME: "Leo, leo",
+    NAV_BASES: "Bases del concurso",
+    NAV_CERTIFICADOS: "Certificados",
+    NAV_COLEGIOS: "Colegios inscritos",
+    NAV_CONCURSO: "Concurso",
+    NAV_GALERIA: "Galeria",
+    NAV_CRITERIOS: "Criterios de lectura",
+    NAV_INFO: "Información para profesores",
+    NAV_INSCRIPCION: "Inscripción",
+    NAV_TEXTOS: "Textos de lectura",
+    NOTFOUND: "No se ha encontrado la página.",
     NOTFOUND_TXT:
-			'Lo sentimos, pero probablemente el link que ha seguido está roto o se ha eliminado la página.',
-    SPONSOR: 'Cooperación y patrocinio: ',
-  },
+      "Lo sentimos, pero probablemente el link que ha seguido está roto o se ha eliminado la página.",
+    SPONSOR: "Cooperación y patrocinio: "
+  }
 }
 
-const getDictionary = (lang = 'es') => dictionary[lang]
+const getDictionary = (lang = "es") => dictionary[lang]
 
 export default getDictionary
