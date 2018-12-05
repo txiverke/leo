34af0533b6bbba354d563b7f0abbf66c83745786
txiverke <txiverke@gmail.com>
2018-12-05 13:32:09 -0500

    Update v: 0.2.9 - Create Semifinal View

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -4,83 +4,118 @@ import { Switch, Route } from 'react-router-dom'
 import RoutesAsync from './RoutesAsync'
 
 const Routes = props => {
-	const { DIC, auth, checkAuth } = props
+  const { DIC, auth, checkAuth } = props
 
-	return (
-		<Switch>
-			<Route
-				exact
-				path="/"
-				component={props => (
-					<RoutesAsync props={props} DIC={DIC} loadingPromise={import('./views/Landing')} />
-				)}
-			/>
+  return (
+    <Switch>
+      <Route
+        exact
+        path='/'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            DIC={DIC}
+            loadingPromise={import('./views/Landing')}
+          />
+        )}
+      />
 
-			<Route
-				path="/admin"
-				component={props => (
-					<RoutesAsync
-						props={props}
-						auth={auth}
-						checkAuth={checkAuth}
-						loadingPromise={import('./views/Admin')}
-					/>
-				)}
-			/>
+      <Route
+        path='/admin'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            auth={auth}
+            checkAuth={checkAuth}
+            loadingPromise={import('./views/Admin')}
+          />
+        )}
+      />
 
-			<Route
-				path="/admin-panel"
-				component={props => (
-					<RoutesAsync
-						props={props}
-						auth={auth}
-						checkAuth={checkAuth}
-						loadingPromise={import('./views/AdminPanel')}
-					/>
-				)}
-			/>
+      <Route
+        path='/admin-panel'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            auth={auth}
+            checkAuth={checkAuth}
+            loadingPromise={import('./views/AdminPanel')}
+          />
+        )}
+      />
 
-			<Route
-				path="/bases-del-concurso"
-				component={props => (
-					<RoutesAsync props={props} DIC={DIC} loadingPromise={import('./views/Bases')} />
-				)}
-			/>
+      <Route
+        path='/bases-del-concurso'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            DIC={DIC}
+            loadingPromise={import('./views/Bases')}
+          />
+        )}
+      />
 
-			<Route
-				path="/textos-de-lectura"
-				component={props => (
-					<RoutesAsync props={props} DIC={DIC} loadingPromise={import('./views/Lectura')} />
-				)}
-			/>
+      <Route
+        path='/textos-de-lectura'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            DIC={DIC}
+            loadingPromise={import('./views/Lectura')}
+          />
+        )}
+      />
 
-			<Route
-				path="/criterios-de-lectura"
-				component={props => (
-					<RoutesAsync props={props} DIC={DIC} loadingPromise={import('./views/Criterios')} />
-				)}
-			/>
+      <Route
+        path='/criterios-de-lectura'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            DIC={DIC}
+            loadingPromise={import('./views/Criterios')}
+          />
+        )}
+      />
 
-			<Route
-				path="/colegios-inscritos"
-				component={props => (
-					<RoutesAsync props={props} DIC={DIC} loadingPromise={import('./views/Colegios')} />
-				)}
-			/>
+      <Route
+        path='/colegios-inscritos'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            DIC={DIC}
+            loadingPromise={import('./views/Colegios')}
+          />
+        )}
+      />
 
-			<Route
-				component={props => (
-					<RoutesAsync props={props} DIC={DIC} loadingPromise={import('./views/NotFound')} />
-				)}
-			/>
-		</Switch>
-	)
+      <Route
+        path='/semifinal-latinoamerica'
+        component={props => (
+          <RoutesAsync
+            props={props}
+            DIC={DIC}
+            loadingPromise={import('./views/Semifinal')}
+          />
+        )}
+      />
+
+      <Route
+        component={props => (
+          <RoutesAsync
+            props={props}
+            DIC={DIC}
+            loadingPromise={import('./views/NotFound')}
+          />
+        )}
+      />
+    </Switch>
+  )
 }
 
 Routes.propTypes = {
-	DIC: PropTypes.object.isRequired,
-	auth: PropTypes.bool.isRequired,
-	checkAuth: PropTypes.func.isRequired,
+  DIC: PropTypes.object.isRequired,
+  auth: PropTypes.bool.isRequired,
+  checkAuth: PropTypes.func.isRequired,
 }
 
 export default Routes
