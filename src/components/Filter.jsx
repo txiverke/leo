b7ed1bcab62380bc59879b3abcf96bab709bd08a
Carles <carles@crix.design>
2019-11-22 15:59:24 +0100

    changed Peru logo

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,14 +1,14 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react'
+import PropTypes from 'prop-types'
 
 class Filter extends React.PureComponent {
   state = {
-    visible: false
+    visible: false,
   }
 
   static propTypes = {
     filter: PropTypes.object.isRequired,
-    handleFilter: PropTypes.func.isRequired
+    handleFilter: PropTypes.func.isRequired,
   }
 
   handleVisibility = () => {
@@ -22,26 +22,25 @@ class Filter extends React.PureComponent {
   render() {
     const { filter } = this.props
     const { visible } = this.state
-    const active = visible ? "active" : ""
+    const active = visible ? 'active' : ''
 
     return (
-      <div className="app-filter">
-        <div className="app-filter-label">
+      <div className='app-filter'>
+        <div className='app-filter-label'>
           <p>Galeria de Images de la edición del </p>
-          <button id="btn_visibility" onClick={this.handleVisibility}>
-            <span>{"Año".toUpperCase()}</span>
-            <span className="icon-arrow-down-circle" />
+          <button id='btn_visibility' onClick={this.handleVisibility}>
+            <span>{'Año'.toUpperCase()}</span>
+            <span className='icon-arrow-down-circle' />
           </button>
           <ul className={`app-filter-list ${active}`}>
             {Object.keys(filter).map(k => (
-              <li className="app-filter-item" key={k}>
+              <li className='app-filter-item' key={k}>
                 <button
                   id={`btn_${k}`}
                   data-year={k}
                   data-count={filter[k]}
                   onClick={this.handleFilter}
-                  disabled={!visible}
-                >
+                  disabled={!visible}>
                   {k}
                 </button>
               </li>
